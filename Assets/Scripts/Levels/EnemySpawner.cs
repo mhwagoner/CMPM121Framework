c7b0517b4c388f86c02b6e0f951ee5feb97f43e4
me333e <alikate458@gmail.com>
2026-04-26 17:55:37 -0700

    added SpawnEnemyType coroutine

--- Assets/Scripts/Levels/EnemySpawner.cs
+++ Assets/Scripts/Levels/EnemySpawner.cs
@@ -74,7 +74,7 @@ public class EnemySpawner : MonoBehaviour
 
 
     IEnumerator SpawnWave()     // needs to spawn each type of enemy in the levels spawn list
-    {                           // and should spawn the sequence with delay found in spawn class
+    {
         GameManager.Instance.state = GameManager.GameState.COUNTDOWN;
         GameManager.Instance.countdown = 3;
         for (int i = 3; i > 0; i--)
@@ -83,40 +83,40 @@ public class EnemySpawner : MonoBehaviour
             GameManager.Instance.countdown--;
         }
         GameManager.Instance.state = GameManager.GameState.INWAVE;
-        // for (int i = 0; i < 10; ++i)
-        // {
-        //     yield return SpawnEnemy();
-        // }
 
-        foreach (var spawn_type in selectedLevel.spawns)    // not sure if this means all the enemy types spawn at the same time or one at a time
-        {                                                   // I think it's one at a time, which is not what we want
+        foreach (var spawn_type in selectedLevel.spawns)
+        {
             currentCount = 0;
             spawn_var["base"] = enemy_types[spawn_type.enemy].hp;
             count = RPNEvaluator.RPNEvaluator.Evaluate(spawn_type.count, spawn_var);
-
-            while(currentCount < count)     // the main loop that spawns the total count of each enemy
-            {
-                foreach (int n in spawn_type.sequence)
-                {
-                    for (int i = 0; i < n; i++)     // spawn as many enemies as values of sequence
-                    {
-                        if (currentCount < count)   // stop spawning if already spawned wave total
-                        {
-                            currentCount += 1;
-                            yield return SpawnEnemy();
-                        }
-                    }
-
-                    yield return new WaitForSeconds(spawn_type.delay);
-                }
-            }
+            yield return SpawnEnemyType(spawn_type);
         }
 
         yield return new WaitWhile(() => GameManager.Instance.enemy_count > 0);
         GameManager.Instance.state = GameManager.GameState.WAVEEND;
     }
 
-    IEnumerator SpawnEnemy()
+    IEnumerator SpawnEnemyType(Spawn spawn_type)    // spawns all enemies of a single type
+    {
+        while(currentCount < count)     // the main loop that spawns the total count of each enemy
+        {
+            foreach (int n in spawn_type.sequence)
+            {
+                for (int i = 0; i < n; i++)     // spawn as many enemies as values of sequence
+                {
+                    if (currentCount < count)   // stop spawning if already spawned wave total
+                    {
+                        currentCount += 1;
+                        SpawnEnemy();
+                    }
+                }
+
+                yield return new WaitForSeconds(spawn_type.delay);
+            }
+        }
+    }
+
+    public void SpawnEnemy()    // spawns single enemy
     {
         SpawnPoint spawn_point = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
         Vector2 offset = Random.insideUnitCircle * 1.8f;
@@ -129,7 +129,6 @@ public class EnemySpawner : MonoBehaviour
         en.hp = new Hittable(50, Hittable.Team.MONSTERS, new_enemy);
         en.speed = 10;
         GameManager.Instance.AddEnemy(new_enemy);
-        yield return new WaitForSeconds(0.5f);
     }
 
     // https://yawgmoth.github.io/CMPM121/slides/lecture4.html#15 :3c
