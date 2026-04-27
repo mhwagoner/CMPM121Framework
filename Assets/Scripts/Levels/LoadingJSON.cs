cb9d2787a257084df3be4617d1c55ee0ddfadfe0
me333e <alikate458@gmail.com>
2026-04-26 18:07:25 -0700

    should loading the enemies and levels be separate? made them a class

---
+++ Assets/Scripts/Levels/LoadingJSON.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+public class LoadingJSON    // could this work? hopefully we could still access everything we need to if we did this
+{
+    private Dictionary<string, Enemy> enemy_types = new Dictionary<string, Enemy>();
+    private Dictionary<string, Level> level_types = new Dictionary<string, Level>();
+
+    // https://yawgmoth.github.io/CMPM121/slides/lecture4.html#15 :3c
+    public void LoadEnemies()
+    {
+        var enemytext = Resources.Load<TextAsset>("enemies");
+        JToken jo = JToken.Parse(enemytext.text);
+        foreach (var enemy in jo)
+        {
+            Enemy en = enemy.ToObject<Enemy>();
+            enemy_types[en.name] = en;
+        }
+    }
+
+    public void LoadLevels()
+    {
+        var leveltext = Resources.Load<TextAsset>("levels");
+        JToken jo = JToken.Parse(leveltext.text);
+        foreach (var level in jo)
+        {
+            Level lvl = level.ToObject<Level>();
+            level_types[lvl.name] = lvl;
+        }
+    }
+}
