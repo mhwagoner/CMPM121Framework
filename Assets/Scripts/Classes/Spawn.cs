a7386526b3372d81a2db712fb26e3cc241bda13f
Mario-2 <67210496+Mario-2@users.noreply.github.com>
2026-04-27 22:46:05 -0700

    Added defaults to Spawn

--- Assets/Scripts/Classes/Spawn.cs
+++ Assets/Scripts/Classes/Spawn.cs
@@ -4,8 +4,9 @@ public class Spawn
 {
     public string enemy;
     public string count;
-    public string hp;
-    public int delay;
-    public int[] sequence;
-    public string location;
+    public string hp = "base";
+    public string damage = "base";
+    public string delay = "2";
+    public int[] sequence = { 1 };
+    public string location = "random";
 }
