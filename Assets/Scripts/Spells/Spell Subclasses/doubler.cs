24ab885243fe8b22ece018dd326180e7dbf496c6
Mario-2 <67210496+Mario-2@users.noreply.github.com>
2026-05-12 16:54:35 -0700

    Implemented doubler modifier

---
+++ Assets/Scripts/Spells/Spell Subclasses/doubler.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class doubler : ModifierSpell
+{
+    private float delay;
+
+    public doubler(SpellCaster owner) : base(owner) { }
+
+    public override void SetAttributes(JToken attributes)
+    {
+        base.SetAttributes(attributes);
+
+        delay = (float) attributes["delay"];
+    }
+
+    public override IEnumerator Cast(Vector3 where, Vector3 target, Hittable.Team team, List<ValueModifier> modifiers)
+    {
+        // Initial cast
+        CoroutineManager.Instance.Run(base.Cast(where, target, team, modifiers));
+
+        // Delay for second cast
+        yield return new WaitForSeconds(delay);
+        yield return base.Cast(where, target, team, modifiers);
+    }
+}
